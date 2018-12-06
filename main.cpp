44e89ceddecac7e768531f0e81cfad2eb972a363
chanami <chanami15@gmail.com>
2018-12-05 23:04:06 -0800

    basic tests

--- main.cpp
+++ main.cpp
@@ -10,17 +10,51 @@ public:
         strcpy(Person::full_name,full_name);
     };
     ~Person() { std::cout << "Person::~Person()\n"; };
+
+    char* get_name(){ return full_name;}
+
 private:
     char full_name[32];
 };
 
-int main() {
+void test_arrow(){
+
+    std::cout << "Test Arrow"<<std::endl;
     UniquePtr<Person> p(new Person("Shlomit"));
     if (p)
     {
-        std::cout << "Hello, Worlpd!"<<std::endl;
+       if(strcmp(p->get_name(), "Shlomit") == 0)
+           std::cout << "Test Arrow Ok"<<std::endl;
+       else
+           std::cout << "Test Arrow FAILED"<<std::endl;
     }
+}
 
-    std::cout << "Hello, World!" <<std::endl;
+void test_bool(){
+    std::cout << "Test Bool"<<std::endl;
+    UniquePtr<Person> p(new Person("Shlomit"));
+    if (p)
+    {
+        std::cout << "Test Bool Ok"<<std::endl;
+    }
+    else
+        std::cout << "Test Bool FAILED"<<std::endl;
+}
+void test_star(){
+    std::cout << "Test Star"<<std::endl;
+    UniquePtr<Person> p(new Person("Shlomit"));
+    if (p)
+    {
+        if(strcmp((*p).get_name(), "Shlomit") == 0)
+            std::cout << "Test Star Ok"<<std::endl;
+        else
+            std::cout << "Test star FAILED"<<std::endl;
+    }
+}
+int main() {
+    test_arrow();
+    test_star();
+    test_bool();
+
     return 0;
 }
