5bc9014c08bfcefc1e7555dff88b7c2f6d97f913
chanami <chanami15@gmail.com>
2018-12-08 23:27:27 -0800

    split main to shared and smart PTR

--- sharedPtr/main.cpp
+++ sharedPtr/main.cpp
@@ -1,41 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstring>
-#include "smart_pointer.h"
 #include "person.h"
 #include "shared_pointer.h"
 
-
-void test_arrow(){
-    UniquePtr<Person> p(new Person("Shlomit"));
-    if (p)
-    {
-       if(strcmp(p->get_name(), "Shlomit") == 0)
-           std::cout << "Test Arrow Ok"<<std::endl;
-       else
-           std::cout << "Test Arrow FAILED"<<std::endl;
-    }
-}
-
-void test_bool(){
-    UniquePtr<Person> p(new Person("Shlomit"));
-    if (p)
-    {
-        std::cout << "Test Bool Ok"<<std::endl;
-    }
-    else
-        std::cout << "Test Bool FAILED"<<std::endl;
-}
-void test_star(){
-    UniquePtr<Person> p(new Person("Shlomit"));
-    if (p)
-    {
-        if(strcmp((*p).get_name(), "Shlomit") == 0)
-            std::cout << "Test Star Ok"<<std::endl;
-        else
-            std::cout << "Test star FAILED"<<std::endl;
-    }
-}
 
 void test_shared_point_arrow(){
     shared_ptr<Person> p(new Person("Shlomit"));
--- uniquePtr/main.cpp
+++ uniquePtr/main.cpp
@@ -3,7 +3,6 @@
 #include <cstring>
 #include "smart_pointer.h"
 #include "person.h"
-#include "shared_pointer.h"
 
 
 void test_arrow(){
